bdefd06aca65d5afd33ec6950a6f1242994714a9
Parsiri Suthiyuth <filmps1@gmail.com>
2024-06-13 23:24:08 -0700

    import openai, langchain

--- ai/main.py
+++ ai/main.py
@@ -1,1 +1,6 @@
-
+import os
+import openai
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
