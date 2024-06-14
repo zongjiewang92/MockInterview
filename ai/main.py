59ac6f06e17dd5feca32e274133afe1be7b17899
Parsiri Suthiyuth <filmps1@gmail.com>
2024-06-13 23:38:44 -0700

    initial conduct_interview function

--- ai/main.py
+++ ai/main.py
@@ -6,3 +6,9 @@ from langchain.prompts import PromptTemplate
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
 
 openai.api_key = '' 
+
+def conduct_interview(extracted_info, company, position):
+    questions = []
+
+    llm = OpenAI(api_key=openai.api_key)
+    candidate_answers = []
