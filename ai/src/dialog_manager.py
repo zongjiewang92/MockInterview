a43dce4778fb799bee7c62bcd711487629a4e44d
filmps <filmps1@gmail.com>
2024-06-22 20:48:23 -0700

    define InterviewerStateMachine, service dialog_manager.py

--- ai/src/dialog_manager.py
+++ ai/src/dialog_manager.py
@@ -3,3 +3,20 @@ import utils
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+
+class InterviewerStateMachine:
+    def __init__(self, company, position, resume_text, extracted_info):
+
+
+
+
+
+
+def service(interviewSM, candidate_input):
+
+
+
+
+if __name__ == "__main__":
+
+    pass
