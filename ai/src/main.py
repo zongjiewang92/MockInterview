6af93346a7aa0b88ef0a66a922e9357f570e826c
filmps <filmps1@gmail.com>
2024-06-25 09:18:26 -0700

    add interview rounds

--- ai/src/main.py
+++ ai/src/main.py
@@ -3,7 +3,6 @@ import utils
 import dialog_manager
 import constant
 
-
 openai.api_key = constant.jyOpenAIKey
 
 # Define Variables
@@ -17,5 +16,10 @@ summary_info, resume_text, extracted_info = utils.parse_resume_file(file_path)
 # Initialize interview state machine
 interviewSM = dialog_manager.InterviewerStateMachine(company, position, resume_text, extracted_info)
 
-
+interviewSM = dialog_manager.service(interviewSM, candidate_input="")
+interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_1_audio.m4a')
+interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_2_audio.m4a')
+interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_3_audio.m4a')
+interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_4_audio.m4a')
+interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_5_audio.m4a')
 
