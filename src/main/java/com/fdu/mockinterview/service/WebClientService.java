6fa7405ea2b91db817eda534f742946b2581198b
zongjiewang92 <zongjiewang92@gmail.com>
2024-07-11 20:21:27 -0700

    update WebClientService

--- src/main/java/com/fdu/mockinterview/service/WebClientService.java
+++ src/main/java/com/fdu/mockinterview/service/WebClientService.java
@@ -1,6 +1,5 @@
 package com.fdu.mockinterview.service;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
