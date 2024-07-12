0e26de73490d813f59d218f36a53ab5ec90a6d4c
Parsiri Suthiyuth <filmps1@gmail.com>
2024-07-12 06:41:23 -0700

    Modify update

--- ai/src/dialog_manager.py
+++ ai/src/dialog_manager.py
@@ -1,19 +1,20 @@
+import os
 import openai
-import utils
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+import utils
+import constant as fsp
+
 
 class InterviewerStateMachine:
-    # Constructor : __init__
     def __init__(self, company, position, resume_text, extracted_info):
-        # set attribute
         self.state = 'INIT'
         self.company = company
         self.position = position
         self.resume_text = resume_text
-        self.extrcted_info = extracted_info
-        self.cadidate_name = extracted_info['Name']
+        self.extracted_info = extracted_info
+        self.candidate_name = extracted_info["Name"]
         self.questions = [
             f"Hi {self.candidate_name}, welcome to {company}, Could you please introduce yourself?",
             f"Why do you want to join {company}?",
@@ -22,24 +23,16 @@ class InterviewerStateMachine:
             "Tell me about a time when you had to learn a new technology or skill quickly. How did you approach it?"
         ]
         self.current_question_index = 0
-        self.cadidate_answers = []
-        self.llm = OpenAI(api_key=openai.api_key, max_tokens=-1)
+        self.candidate_answers = []
+        self.llm = OpenAI(api_key=openai.api_key, max_tokens=1000)
 
-    # state : INIT, WELCOME, ASK_QUESTION, EVALUATE 
-    def next_state(self, user_input=None):
+    def next_state(self, user_input=None, ai_output_dir="../output"):
         if self.state == 'INIT':
             # print(f'Current state {self.state}')
             self.state = 'ASK_QUESTION'
             # print(f'Next state {self.state}')
-            return self._ask_question()
-        
-        elif self.state == 'WELCOME':
-            # print(f'Current state {self.state}')
-            self.candidate_answers.append(user_input)
-            self.state = 'ASK_QUESTION'
-            # print(f'Next state {self.state}')
-            return self._ask_question()
-        
+            return self._ask_question(ai_output_dir)
+
         elif self.state == 'ASK_QUESTION':
             # print(f'Current state {self.state}')
             self.candidate_answers.append(user_input)
@@ -47,21 +40,49 @@ class InterviewerStateMachine:
             # print(self.current_question_index, len(self.questions))
             if self.current_question_index < len(self.questions):
                 # print(f'Next state {self.state}')
-                return self._ask_question()
+                return self._ask_question(ai_output_dir)
             else:
                 self.state = 'EVALUATE'
                 # print(f'Next state {self.state}')
-                return self._evaluate()
+                return self._evaluate(ai_output_dir)
 
-    def _ask_question(self):
+    def _ask_question(self, ai_output_dir):
         question = self.questions[self.current_question_index]
+        # Add contextual connection
+        if self.current_question_index > 0:
+            previous_answer = self.candidate_answers[-1]
+            context_connector = self._generate_context_connector(previous_answer, question)
+            question = f"{context_connector} {question}"
         response_text = question
-        return utils.call_tts_api(response_text, f"../output/R{self.current_question_index}_response.mp3")
+        return response_text, utils.call_tts_api(response_text, os.path.join(ai_output_dir, f"R{self.current_question_index}_response.mp3"))
 
-    def _evaluate(self):
+
+    def _generate_context_connector(self, previous_answer, next_question):
+        prompt_template_connector = PromptTemplate(
+            input_variables=["previous_answer", "next_question"],
+            template="""
+            Based on the candidate's previous answer:
+
+            {previous_answer}
+
+            Generate a sentence to smoothly transition to the next question:
+
+            {next_question}
+
+            The transition sentence should be relevant to the previous answer and lead naturally into the next question.
+
+            Please remember that you are the interviewer, and you are interviewing the candidate. Please pay attention to using pronouns like 'you' or 'I' accordingly.
+            """
+        )
+        chain = LLMChain(prompt=prompt_template_connector, llm=self.llm)
+        context_connector = chain.run({"previous_answer": previous_answer, "next_question": next_question})
+        return context_connector.strip()
+    
+
+    def _evaluate(self, ai_output_dir):
         answers = "\n".join([f"Q: {self.questions[i]}\nA: {answer}" for i, answer in enumerate(self.candidate_answers)])
         prompt_template_evaluation = PromptTemplate(
-            input_variables=["info", "answers"],
+            input_variables=["info", "answers", "good_example", "average_example", "bad_example"],
             template="""
             You are a mock interviewer. The candidate's resume information is as follows:
 
@@ -71,34 +92,39 @@ class InterviewerStateMachine:
 
             {answers}
 
-            Now, please first assign a score to the candidate's mock interview performance out of a total of 10 points. Then, provide some feedback and guidance based on the interaction during the interview. You may also suggest how the candidate could improve their responses based on specific details from their resume.
+            Here are some good example for you to reference: {good_example}.
+            Here are some average example for you to reference: {average_example}.
+            Here are some bad example for you to reference: {bad_example}.
+
+            Based on the candidate's mock interview performance and interaction during the interview, as well as the few-shot examples you have learned, please provide some feedback and guidance to the candidate. You may also suggest how the candidate could improve their responses based on specific details from their resume.
 
             """
         )
         chain = LLMChain(prompt=prompt_template_evaluation, llm=self.llm)
-        evaluation_result = chain.run({"info": self.extracted_info, "answers": answers})
-        return utils.call_tts_api(evaluation_result, f"../output/R{self.current_question_index}_response.mp3")
+        evaluation_result = chain.run({"info": self.extracted_info, "answers": answers, "good_example": fsp.good_example, "average_example": fsp.average_example, "bad_example": fsp.bad_example})
+        return evaluation_result, utils.call_tts_api(evaluation_result, os.path.join(ai_output_dir, f"R{self.current_question_index}_response.mp3"))
 
 
-def service(interviewSM, candidate_input):
+def service(interviewSM, candidate_input, ai_output_dir):
     # Simulate interaction with backend
     if interviewSM.state == 'INIT':
         # Start interviewSM
-        audio_response_path = interviewSM.next_state()
+        ai_text, audio_response_path = interviewSM.next_state()
+        user_input_text = "no input"
     elif interviewSM.state == 'EVALUATE':
-        audio_response_path = interviewSM._evaluate()
+        ai_text, audio_response_path = interviewSM._evaluate()
     else:
         # Simulate receiving user audio input from backend
         user_audio_path = candidate_input
         user_input_text = utils.call_asr_api(user_audio_path)
-        audio_response_path = interviewSM.next_state(user_input_text)
+        ai_text, audio_response_path = interviewSM.next_state(user_input_text)
 
     # Send audio_response_path to backend
     print(f"AI respones are store in : {audio_response_path}")
-    return interviewSM
+    return interviewSM, ai_text, user_input_text
 
 
-# if run this file directly : Ture (__name__ เป็นตัวแปรพิเศษใน Python ที่จะมีค่าเป็น "__main__" เมื่อไฟล์นั้นถูกรันโดยตรง แต่ถ้าRunจากไฟล์อื่น จะมีค่าเป็นชื่อของไฟล์ที่ import เข้ามา)
-# if import this file : False
 if __name__ == "__main__":
+
     pass
+
--- ai/src/main.py
+++ ai/src/main.py
@@ -1,9 +1,13 @@
+import os
 import openai
 import utils
 import dialog_manager
 import constant
 
-openai.api_key = constant.jyOpenAIKey
+from dotenv import load_dotenv
+load_dotenv()
+MyOpenaiKey = os.getenv("MyOpenaiKey")
+openai.api_key = MyOpenaiKey
 
 # Define Variables
 company = "Tech Innovations Inc."
@@ -15,11 +19,24 @@ summary_info, resume_text, extracted_info = utils.parse_resume_file(file_path)
 
 # Initialize interview state machine
 interviewSM = dialog_manager.InterviewerStateMachine(company, position, resume_text, extracted_info)
+# Simulated 5 round conversation.
+interviewSM, ai_resp_text, user_input_text = dialog_manager.service(interviewSM, candidate_input='', ai_output_dir="../output")
+print(f"Round0: Candidate do not have input:{user_input_text}")
+print(f"Round1: AI question: {ai_resp_text}")
+interviewSM, ai_resp_text, user_input_text = dialog_manager.service(interviewSM, candidate_input='../data/User_1_audio.m4a', ai_output_dir="../output")
+print(f"Round1: Candidate audio asr result:{user_input_text}")
+print(f"Round2: AI question: {ai_resp_text}")
+interviewSM, ai_resp_text, user_input_text = dialog_manager.service(interviewSM, candidate_input='../data/User_2_audio.m4a', ai_output_dir="../output")
+print(f"Round2: Candidate audio asr result:{user_input_text}")
+print(f"Round3: AI question: {ai_resp_text}")
+interviewSM, ai_resp_text, user_input_text = dialog_manager.service(interviewSM, candidate_input='../data/User_3_audio.m4a', ai_output_dir="../output")
+print(f"Round3: Candidate audio asr result:{user_input_text}")
+print(f"Round4: AI question: {ai_resp_text}")
+interviewSM, ai_resp_text, user_input_text = dialog_manager.service(interviewSM, candidate_input='../data/User_4_audio.m4a', ai_output_dir="../output")
+print(f"Round4: Candidate audio asr result:{user_input_text}")
+print(f"Round5: AI question: {ai_resp_text}")
+interviewSM, ai_resp_text, user_input_text = dialog_manager.service(interviewSM, candidate_input='../data/User_5_audio.m4a', ai_output_dir="../output")
+print(f"Round5: Candidate audio asr result:{user_input_text}")
+print(f"RoundE: AI evaluation: {ai_resp_text}")
 
-interviewSM = dialog_manager.service(interviewSM, candidate_input="")
-interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_1_audio.m4a')
-interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_2_audio.m4a')
-interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_3_audio.m4a')
-interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_4_audio.m4a')
-interviewSM = dialog_manager.service(interviewSM, candidate_input='../data/User_5_audio.m4a')
 
